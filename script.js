026f9d52abe846fec783eaf16e0128936a03aa36
mounga <christopher.fourgeaud@gmail.com>
2017-08-02 16:03:41 +0200

    consigne a à 4 effectuer

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("test");
+var tour = 0;
+
+$(".case").click(function(){
+
+	if (tour%2 === 0) {
+		($(this).html("<img src='javascript.png' alt='js'>"));
+	} else{
+		($(this).html("<img src='semantic.png' alt='js'>"));
+	}tour++;
+	
+
+});
+
+
+
+var turn
