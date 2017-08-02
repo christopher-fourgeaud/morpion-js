e0df04f713b0841a5d999e70c12840ac21715249
mounga <christopher.fourgeaud@gmail.com>
2017-08-02 17:01:02 +0200

    Ajout des joueurs, une image / case et bouton clear

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 console.log("test");
 var tour = 0;
+var joueur1 = prompt("Nom du joueur 1 :")
+var joueur2 = prompt("Nom du joueur 2 :")
 
 $(".case").click(function(){
+	if ($(this).html() === "") {		
+		if (tour%2 === 0) {
+			($(this).html("<img src='javascript.png' alt='js'img-responsive center-block>"));
+		} else{
+			($(this).html("<img src='semantic.png' alt='js'img-responsive center-block>"));
+		}tour++;
 
-	if (tour%2 === 0) {
-		($(this).html("<img src='javascript.png' alt='js'>"));
-	} else{
-		($(this).html("<img src='semantic.png' alt='js'>"));
-	}tour++;
-	
-
+	}
 });
 
+$(".joueurs").html(joueur1 + " VS " + joueur2);
 
-
-var turn
+$(".clear").click(function(){
+	$(".case").html("");
+	tour = 0;
+});
