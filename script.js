94664a30a2f711d6a8b38dcdb030a7561dfce927
mounga <christopher.fourgeaud@gmail.com>
2017-08-03 11:35:12 +0200

    modif boutton + ajout des datas dans un tableau

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
-console.log("test");
-var tour = 0;
-var joueur1 = prompt("Nom du joueur 1 :")
-var joueur2 = prompt("Nom du joueur 2 :")
 
+//====== Définition des variables ============//
+var tour = 0;
+var position =[];
+// var joueur1 = prompt("Nom du joueur 1 :");
+// var joueur2 = prompt("Nom du joueur 2 :");
+
+//== Click function pour afficher les images===//
 $(".case").click(function(){
-	if ($(this).html() === "") {		
+	if ($(this).html() === "") {
 		if (tour%2 === 0) {
 			($(this).html("<img src='javascript.png' alt='js'img-responsive center-block>"));
 		} else{
 			($(this).html("<img src='semantic.png' alt='js'img-responsive center-block>"));
 		}tour++;
-
+		position.push($(this).data("col"));
+		console.log(position)
 	}
+	
 });
 
-$(".joueurs").html(joueur1 + " VS " + joueur2);
 
+//============= Affiche le nom des joueurs======//
+// $(".joueurs").html(joueur1 + " VS " + joueur2);
+
+//====================== Bouton reset ===========//
 $(".clear").click(function(){
 	$(".case").html("");
 	tour = 0;
 });
+
+//====================== TEST ====================//
