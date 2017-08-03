d6629d6bdcae18a7b00f5f272b41a92e49fd7e65
mounga <christopher.fourgeaud@gmail.com>
2017-08-03 14:37:03 +0200

    Ajout des conditions de victoires

--- script.js
+++ script.js
@@ -1,22 +1,61 @@
 
 //====== Définition des variables ============//
 var tour = 0;
-var position =[];
+var joueur1 = 1;
+var joueur2 = 2;
+var position =["0","0","0","0","0","0","0","0","0"];
 // var joueur1 = prompt("Nom du joueur 1 :");
 // var joueur2 = prompt("Nom du joueur 2 :");
+var data;
+
+//======Fonction pour la condition de vicctoire=//
+function condition(arg1) {	
+	if(position[0] === arg1 && position[1] === arg1 && position[2] === arg1) {
+		alert("Joueur" +" "+ arg1+ " " + "gagne!");
+	}
+	if(position[3] === arg1 && position[4] === arg1 && position[5] === arg1) {
+		alert("Joueur" +" "+ arg1+ " " + "gagne!");
+	}
+	if(position[6] === arg1 && position[7] === arg1 && position[8] === arg1) {
+		alert("Joueur" +" "+ arg1+ " " + "gagne!");
+	}
+	if(position[0] === arg1 && position[3] === arg1 && position[6] === arg1) {
+		alert("Joueur" +" "+ arg1+ " " + "gagne!");
+	}
+	if(position[1] === arg1 && position[4] === arg1 && position[7] === arg1) {
+		alert("Joueur" +" "+ arg1+ " " + "gagne!");
+	}
+	if(position[2] === arg1 && position[5] === arg1 && position[8] === arg1) {
+		alert("Joueur" +" "+ arg1+ " " + "gagne!");
+	}
+	if(position[0] === arg1 && position[4] === arg1 && position[8] === arg1) {
+		alert("Joueur" +" "+ arg1+ " " + "gagne!");
+	}
+	if(position[2] === arg1 && position[4] === arg1 && position[6] === arg1) {
+		alert("Joueur" +" "+ arg1+ " " + "gagne!");
+	}
+
+}
 
 //== Click function pour afficher les images===//
 $(".case").click(function(){
+	data = ($(this).data("col")) - 1;
 	if ($(this).html() === "") {
 		if (tour%2 === 0) {
-			($(this).html("<img src='javascript.png' alt='js'img-responsive center-block>"));
+			($(this).html("<img src='javascript.png' alt='js'>"));
+			position[data] = joueur1;
 		} else{
-			($(this).html("<img src='semantic.png' alt='js'img-responsive center-block>"));
+			($(this).html("<img src='semantic.png' alt='semantic'>"));
+			position[data] = joueur2;
 		}tour++;
-		position.push($(this).data("col"));
-		console.log(position)
+		
 	}
-	
+	condition(joueur1);
+	condition(joueur2);
+		if (tour === 9) {
+		alert("MATCH NUL");
+	}
+
 });
 
 
@@ -27,6 +66,7 @@ $(".case").click(function(){
 $(".clear").click(function(){
 	$(".case").html("");
 	tour = 0;
+	position =["0","0","0","0","0","0","0","0","0"]
 });
 
 //====================== TEST ====================//
