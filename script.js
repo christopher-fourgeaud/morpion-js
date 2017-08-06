0e0bbbe726b37d2e1e7ad74d135253a6e558f129
Christopher Fourgeaud <Christopher.fourgeaud@gmail.com>
2017-08-06 20:35:37 +0200

    re-creation du morpion + ajout des scores

--- script.js
+++ script.js
@@ -1,5 +1,252 @@
+// Mes variables
+var nomJoueur1 = "";
+var nomJoueur2 = "";
+var nbParties = 1;
+var o = "<img class='o' src='o.png' alt='o'>";
+var x = "<img class='x' src='x.png' alt='x'>";
+var compteur = 0;
+var scoreJoueur1 = 0;
+var scoreJoueur2 = 0;
+var joueur1 = 1;
+var joueur2 = 2;
+var data;
+var position = [0,0,0,0,0,0,0,0,0];
 
-//====== Définition des variables ============//
+// Verification de victoire
+function condition(arg1, arg2, arg3){
+	if (position[0] === arg1 && position[1] === arg1 && position[2] === arg1
+	||  position[3] === arg1 && position[4] === arg1 && position[5] === arg1
+	||  position[6] === arg1 && position[7] === arg1 && position[8] === arg1
+	||  position[0] === arg1 && position[3] === arg1 && position[6] === arg1
+	||  position[1] === arg1 && position[4] === arg1 && position[7] === arg1
+	||  position[2] === arg1 && position[5] === arg1 && position[8] === arg1
+	||  position[0] === arg1 && position[4] === arg1 && position[8] === arg1
+	||  position[2] === arg1 && position[4] === arg1 && position[6] === arg1) 
+	{	  
+	alert("Bravo : " + arg2 + " gagne la partie");
+	arg3++;
+	reset();
+	}
+	return arg3;	
+}
+
+// Fonction reset
+function reset(){
+	compteur = 0;
+	position = [0,0,0,0,0,0,0,0,0]
+	$(".case").html("");
+}
+
+// Ma fonction clique
+$(".case").click(function(){
+	data = (($(this).data("col")));
+	if ($(this).html() === "" ) {
+		if (compteur%2 === 0) {
+			($(this)).html(o);
+			compteur++;
+			position[data] = joueur1;			
+			scoreJoueur1 = condition(joueur1, nomJoueur1, scoreJoueur1);
+			
+		} else {
+			($(this)).html(x);
+			compteur++;
+			position[data] = joueur2;
+			scoreJoueur2 = condition(joueur2, nomJoueur2, scoreJoueur2);
+
+		}
+		if (compteur === 9) {
+			alert("MATCH NUL");
+			reset();
+		}
+		
+	}// Affichage des scores
+	$(".scoreJoueur1").html(scoreJoueur1)
+	$(".scoreJoueur2").html(scoreJoueur2)
+})
+
+// Bouton reset
+$(".reset").click(function(){
+	$(".case").html("");
+	scoreJoueur1 = 0;
+	scoreJoueur2 = 0;
+	$(".scoreJoueur1").html(scoreJoueur1)
+	$(".scoreJoueur2").html(scoreJoueur2)
+	reset();
+})
+
+// Affichage au chargement de la page des paramètres des parties ====//
+$("#newGame").modal("toggle");
+
+// Bouton Nouvelle partie
+$("#nouvellePartie").click(function(){
+	$("#newGame").modal("toggle");
+})
+
+// Bouton valider du menu nouvelle partie
+$("#parametres").click(function(){
+	nomJoueur1 = $(".player1").val();
+	nomJoueur2 = $(".player2").val();
+	nbParties = $(".joueur1").val();
+	$(".nomJoueur1").html(nomJoueur1);
+	$(".nomJoueur2").html(nomJoueur2);
+	scoreJoueur1 = 0;
+	scoreJoueur2 = 0;
+	$(".scoreJoueur1").html(scoreJoueur1)
+	$(".scoreJoueur2").html(scoreJoueur2)
+	reset();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*//====== Définition des variables ============//
 var tour = 0;
 var joueur1 = 1;
 var joueur2 = 2;
@@ -97,3 +344,4 @@ $(".clear").click(function(){
 });
 
 //====================== TEST ====================//
+*/
